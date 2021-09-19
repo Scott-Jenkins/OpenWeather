323f58c6eb158f13d1c02ae91028d36b9bf3a079
Scott-Jenkins <56408180+Scott-Jenkins@users.noreply.github.com>
2021-09-19 11:37:42 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -13,13 +13,11 @@ function showPosition(position) {
   let longitude = position.coords.longitude
 
   let localUrl = "https://api.openweathermap.org/data/2.5/weather?APPID=ea8837df503db1cc47357bc3289f366e&lat="+ latitude +"&lon="+ longitude +""
-  console.log(localUrl)
 
   fetch(localUrl)
     .then(response => response.json())
     .then(content => {
       
-      console.log(content.name);
       document.getElementById("search").value = content.name;
       RefreshApi();
       Forecast()
@@ -42,20 +40,19 @@ function RunApi() {
   });
 }
 
-function RefreshApi() {
+function RefreshApi(UnitAppend = "°C") {
   //ev.preventDefault(); //to stop the page reload
   let str = document.getElementById("search").value.trim();
   $(".section.results").show();
   let url = "https://api.openweathermap.org/data/2.5/weather?q="+str+"&appid="+ APIKEY +"&units=" + units;
-  console.log(url);
   fetch(url)
     .then(response => response.json())
     .then(content => {
       
       let tempLow = document.querySelector("#temp-low");
       let tempHigh = document.querySelector("#temp-high");
-      tempLow.textContent = Math.trunc(content.main.temp_min) + "°C";
-      tempHigh.textContent = Math.trunc(content.main.temp_max) + "°C";
+      tempLow.textContent = Math.trunc(content.main.temp_min) + UnitAppend;
+      tempHigh.textContent = Math.trunc(content.main.temp_max) + UnitAppend;
       
 
       let weather = document.querySelector("#weather");
@@ -65,7 +62,7 @@ function RefreshApi() {
       location.textContent = content.name;
 
       let feelsLike = document.querySelector("#feels-like");
-      feelsLike.textContent = "feels like " + Math.trunc(content.main.feels_like) + "°C";
+      feelsLike.textContent = "feels like " + Math.trunc(content.main.feels_like) + UnitAppend;
 
       let humidity = document.querySelector("#humidity");
       humidity.textContent = "humidity: "+content.main.humidity + "%";
@@ -73,30 +70,6 @@ function RefreshApi() {
       let icon = document.querySelector("#icon");
       icon.src = "http://openweathermap.org/img/wn/"+ content.weather[0].icon + ".png";
       document.querySelector("#location h1").appendChild(icon); 
-
-      //----------------------Sunset Sunrise-----------------------------
-
-      //let sunrise_timestamp = content.sys.sunrise;
-      // Create a new JavaScript Date object based on the timestamp
-      // multiplied by 1000 so that the argument is in milliseconds, not seconds.
-      //var sunrise_date = new sunrise_date(sunrise_timestamp * 1000);
-      // Hours part from the timestamp
-      //var sunrise_hours = sunrise_Date.getHours();
-      // Minutes part from the timestamp
-      //var sunrise_minutes = "0" + sunrise_date.getMinutes();
-      // Seconds part from the timestamp
-      //var sunrise_seconds = "0" + sunrise_date.getSeconds();
-
-      // Will display time in 10:30:23 format
-      //var sunrise_formattedTime = sunrise_hours + ':' + sunrise_minutes.substr(-2) + ':' + sunrise_seconds.substr(-2);
-
-      //let sunrise = document.querySelector("#sunrise");
-      //sunrise.textContent = sunrise_formattedTime;
-
-      
-
-      //let sunset = document.querySelector("#sunset");
-      //sunset.textContent = content.sys.sunset;
 
       //---------------------Change Background-------------------------
       switch (weather.textContent) {
@@ -141,44 +114,62 @@ document.querySelector("#dropdownMenuButton").addEventListener("click", ev => {
   switch (unitsValue) {
     case "°C":
       units = "metric"
-      RefreshApi()
+      RefreshApi("°C")
+      Forecast(units, "°C")
       break;
     case "°F":
       units = "imperial"
-      RefreshApi()
+      RefreshApi("°F")
+      Forecast(units, "°F")
       break;
     case "°K":
       units = "standard"
-      RefreshApi()
+      RefreshApi("°K")
+      Forecast(units, "°K")
       break;
   }
 });
 
 
-function Forecast() {
+function Forecast(units = "metric", unitsAppend = "°C") {
   let str = document.getElementById("search").value.trim();
   let ForecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+str+"&appid="+ APIKEY +"&units=" + units;
-  console.log(ForecastUrl)
   fetch(ForecastUrl)
     .then(response => response.json())
     .then(content => {
-      /*
-      let getWeatherDate = content.list[0].dt
 
-      getDate = getDate * 1000
-      console.log(getDate);
-      let newDate = getDate.();
-      console.log(newDate);
-      */
-      let weatherTimestamp = content.list[0].dt;
+      var forecastArea = document.querySelector(".section.forecast .container .row")
+      forecastArea.innerHTML = ''
 
-      var getWeatherDate = weatherTimestamp * 1000;
+      for (let index = 8; index < 40; index = index + 8) {
 
-      var getWeatherDate_hours = getWeatherDate.getHours();
+        let timeStamp = content.list[index].dt;
 
-      var getWeatherDate_minutes = "0" + getWeatherDate.getMinutes();
+        let date = new Date(timeStamp*1000);
 
-      var getWeatherDate_seconds = "0" + getWeatherDate.getSeconds();
+        // Month
+        var months_arr = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+        var month =  months_arr[date.getMonth()];
+
+        // Day
+        var day = date.getDate();
+
+        let temp = Math.trunc(content.list[index].main.temp);
+        let weather = content.list[index].weather[0].main;
+        let dateText = content.list[index].dt_txt;
+
+        let icon = "http://openweathermap.org/img/wn/"+ content.list[index].weather[0].icon + ".png";
+
+
+        
+
+        
+        var newDay = document.createElement("div")
+        newDay.className = "col-md text-center"
+        newDay.innerHTML = '<p>'+ temp + unitsAppend +'</p><img src='+ icon +'><hr><p>'+ weather +'</p><p><b>'+ day +' '+ month +'</b></p>'
+        forecastArea.appendChild(newDay)
+      }
+
     })
     .catch(err => {
       console.error(err);
